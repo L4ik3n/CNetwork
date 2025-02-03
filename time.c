0bb354ac20d5d4a19861831e94461822931616e2
Laiken <laike@vic.local>
2025-02-03 21:44:02 +0100

    simple time server

---
+++ time.c
@@ -0,0 +1,93 @@
+/* TIME SERVER (UNIX-BASED)*/
+
+
+#define _GNU_SOURCE
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+ 
+
+
+int main() {
+
+    // configure local address for web server to bind to 
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address; 
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    // create socket 
+    int socket_listen;
+    socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
+    if (!(socket_listen >=0)){
+        fprintf(stderr, "call to socket() failed. (%d)\n", errno);
+        return 1;
+    }
+
+    // bind socket to local address (bind returns 0 on success, non-zero on failure)
+    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)){
+        fprintf(stderr, "call to bind() failed. (%d)\n", errno);
+        return 1;
+    }
+    freeaddrinfo(bind_address);
+
+    // start listening 
+    if (listen(socket_listen, 10) < 0){
+        fprintf(stderr, "call to listen() failed. (%d)\n", errno);
+        return 1;
+    }
+
+    // accept incoming connections 
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+    int socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+    if (!(socket_client >= 0)){
+        fprintf(stderr, "call to accept() failed. (%d)\n", errno);
+        return 1;
+    }
+
+    // log client address 
+    char address_buffer[100];
+    getnameinfo((struct sockaddr*)&client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+    printf("%s\n", address_buffer);
+
+
+    // read request 
+    char request[200];
+    int bytes_received = recv(socket_client, request, 1024, 0);
+    printf("Received %d bytes. \n", bytes_received);
+    printf("%.*s", bytes_received, request);
+
+    // send response
+    const char *response = 
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Type: text/plain\r\n\r\n"
+        "Local time is: ";
+    int bytes_sent = send(socket_client, response, strlen(response), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+
+    time_t seconds;
+    time(&seconds);
+    char *time_msg = ctime(&seconds);
+    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+
+    close(socket_client);
+
+    return 0; 
+
+
+
+}
