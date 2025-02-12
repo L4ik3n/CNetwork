35e822766ba558c06d7fda27e2ff6fef977798dc
Laiken <grz130gorz@gmail.com>
2025-02-12 22:10:57 +0100

    comments

--- tcp_client.c
+++ tcp_client.c
@@ -1,4 +1,4 @@
-/* TCP CLIENT FOR TESTING */
+/* TCP/UDP CLIENT FOR TESTING */
 
 
 #define _GNU_SOURCE
@@ -14,7 +14,7 @@
 #include <ctype.h>
 
 
-
+// Ensure correct numbers of arguments are provided
 int main(int argc, char *argv[]){
     if (argc < 4) {
         fprintf(stderr, "Usage: filename hostname service protocol(TCP/UDP)");
@@ -22,6 +22,7 @@ int main(int argc, char *argv[]){
 
     }
 
+// Convert provided protocol to uppercase 
     char protocol [4];
     protocol[3] = '\0'; 
     int i;
@@ -29,6 +30,7 @@ int main(int argc, char *argv[]){
         protocol[i] = toupper(argv[3][i]); 
     }
 
+    // Configure socket hints 
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     if (!(strcmp(protocol, "TCP")))
@@ -39,13 +41,15 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Usage: filename hostname service protocol(TCP/UDP)");
         return 1;
     }
+
+    // Get addres information for peer 
     struct addrinfo *server_addr;
     if (getaddrinfo(argv[1], argv[2], &hints, &server_addr)){
         fprintf(stderr, "call to getaddrinfo() failed. (%d)\n", errno);
         return 1;
     }
 
-
+    // Create socket 
     int socket_server;
     socket_server = socket(server_addr->ai_family, server_addr->ai_socktype, server_addr->ai_protocol);
     if (!(socket_server >= 0)){
@@ -54,28 +58,31 @@ int main(int argc, char *argv[]){
     }
     
 
-
+    // Establish connection 
     if (connect(socket_server, server_addr->ai_addr, server_addr->ai_addrlen)){
         fprintf(stderr, "call to connect() failed. (%d)\n", errno);
         return 1;
     }
 
+    
     freeaddrinfo(server_addr);
 
     printf("Enter the messge u wish to send to server and confirm by pressing enter. \n");
 
+
+    // Wait for user input or incoming data 
     while(1){
         fd_set reads;
         FD_ZERO(&reads);
         FD_SET(socket_server, &reads);
         FD_SET(0, &reads);
 
-
         if (select(socket_server+1, &reads, 0, 0, 0) < 0 ){
             fprintf(stderr, "call to select() failed. (%d)", errno);
             return 1;
         }
 
+        // Check for received data  
         if (FD_ISSET(socket_server, &reads)){
             char read[4096];
             int bytes_received = recv(socket_server, read, 4096, 0);
@@ -86,7 +93,7 @@ int main(int argc, char *argv[]){
             printf("Received (%d bytes): %.*s", bytes_received, bytes_received, read);
 
         }
-
+        // Check for user input  
         if (FD_ISSET(0, &reads)) {
             char read[4096];
             if (!fgets(read, 4096, stdin)) break;
