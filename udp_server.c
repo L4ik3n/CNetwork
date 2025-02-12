fa0d6f10d3fbcea46d6625657044701e8c6777c0
Laiken <grz130gorz@gmail.com>
2025-02-12 22:20:51 +0100

    added dual stack

--- udp_server.c
+++ udp_server.c
@@ -12,13 +12,16 @@
 
 int main(){
 
+    // Configure socket hints for dual stack 
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_socktype = SOCK_DGRAM;
-    
+    hints.ai_flags = AI_PASSIVE;
     struct addrinfo *bind_addr;
     getaddrinfo(0,"8080", &hints, &bind_addr);
+    
 
+    // Create socket
     int socket_listen;
     socket_listen = socket(bind_addr->ai_family, bind_addr->ai_socktype, bind_addr->ai_protocol);
 
@@ -27,7 +30,9 @@ int main(){
         return 1;
 
     }
+    
 
+    // Bind socket to address 
     if (bind(socket_listen, bind_addr->ai_addr, bind_addr->ai_addrlen)){
         fprintf(stderr, "call to bind() failed. (%d)\n", errno);
         return 1; 
@@ -40,15 +45,16 @@ int main(){
     FD_SET(socket_listen, &mfds);
     int max_socket = socket_listen; 
 
-
+    
+    // Wait for incoming data and echo 
     while(1){
         fd_set rfds;
-        rfds= mfds; 
+        rfds = mfds; 
         if (select(max_socket + 1, &rfds, 0, 0, 0) < 0){
             fprintf(stderr, "call to select() failed. (%d)\n", errno);
             return 1;
         }
-        
+        // Read and print data 
         if (FD_ISSET(socket_listen, &rfds)){
             struct sockaddr_storage client_addr;
             socklen_t client_len = sizeof(client_addr);
@@ -58,8 +64,9 @@ int main(){
                 fprintf(stderr, "connection closed. (%d)\n", errno);
                 return 1;
             }
-            printf("Received %d bytes: %.*s", bytes_received, bytes_received, read);
+            printf("Received %d bytes: %.*s\n", bytes_received, bytes_received, read);
 
+            // Send back 
             sendto(socket_listen, read, bytes_received, 0, (struct sockaddr*) &client_addr, client_len); 
 
         }
