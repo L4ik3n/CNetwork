a99ca04037f680dcd3fb04ca3816bd942bf35dba
Laiken <grz130gorz@gmail.com>
2025-02-12 22:17:16 +0100

    now properly echoes

--- chat.c
+++ chat.c
@@ -12,6 +12,7 @@
 
 int main() {
 
+    // Configure socket hints and get address
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -21,6 +22,8 @@ int main() {
     struct addrinfo *bind_addr;
     getaddrinfo(0, "8080", &hints, &bind_addr);
 
+
+    // Create socket
     int socket_listen;
     socket_listen = socket(bind_addr->ai_family, bind_addr->ai_socktype, bind_addr->ai_protocol);
 
@@ -29,6 +32,7 @@ int main() {
         return 1;
     }
 
+    // Bind socket to address  
     if (bind(socket_listen, bind_addr->ai_addr, bind_addr->ai_addrlen)){
         fprintf(stderr, "call to bind() failed. (%d)", errno);
         return 1; 
@@ -36,6 +40,7 @@ int main() {
 
     freeaddrinfo(bind_addr);
 
+    // Listen for connections 
     if (listen(socket_listen, 10) < 0){
         fprintf(stderr, "call to listen() failed. (%d)\n", errno);
         return 1;
@@ -46,17 +51,21 @@ int main() {
     FD_SET(socket_listen, &mfds);
     int max_socket = socket_listen;
     
-
+    // Wait for incoming data and echo it to other clients excluding sender 
     while(1){
         fd_set rfds;
         rfds = mfds; 
+
 
         if (select(max_socket + 1, &rfds, 0, 0, 0) < 0){
             fprintf(stderr, "call to select() failed. (%d)\n", errno);
             return 1;
         }
         int i; 
+
+        // Loop thru all sockets 
         for(i = 1; i<=max_socket; ++i){
+            // Check for new connections 
             if (FD_ISSET(i, &rfds)) {
                 if (i == socket_listen) {
                     struct sockaddr_storage client_addr;
@@ -70,7 +79,9 @@ int main() {
                     if (socket_client > max_socket) max_socket = socket_client;
                 
 
-                } else  {
+                } 
+            // Read received data and echo it to other clients 
+            else{
                     char read[1024];
                     int bytes_received = recv(i, read, 1024, 0);
                     if (bytes_received < 1){
@@ -81,13 +92,13 @@ int main() {
                 int j;
                 for(j = 1; j <= max_socket; ++j){
                     if (FD_ISSET(j, &mfds)){
-                        if (j == socket_listen || j == 1)
+                        if (j == socket_listen || j == i)
                             continue;
                         else 
                             send(j, read, bytes_received, 0);
                     }
                 }
-                }
+            }
 
 
 
